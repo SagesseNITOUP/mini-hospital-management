24ea2aeeeeae1e8a934d989d59702a71a8575cca
lioneljospin <kennenzangem@gmail.com>
2025-05-14 16:21:31 +0200

    create_account

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/patient/create', function () {
+    return view('patient.create');
 });
